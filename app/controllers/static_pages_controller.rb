89712dc135f6b1c5b5c0234246eaac6173636ea9
asterather <asterather@gmail.com>
2015-01-17 01:42:29 -0600

    first commit with basic website setup.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+	def index
+	end
+
+	def admin
+	end
+
+	def about
+	end
+	
+end
---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Website::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  get 'static_pages/admin'
+  get 'static_pages/about'
+  get 'static_pages/index'
+  root 'static_pages#index'
+  
+  match '/admin', to: 'static_pages#admin',    via: 'get'
+  match '/about', to: 'static_pages#about',    via: 'get'
+  match '/index', to: 'static_pages#index',    via: 'get'
+
+
+end
